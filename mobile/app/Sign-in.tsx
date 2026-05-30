3cfdaa43b09a618373597971fb31aa4993401da8
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-30 23:41:49 +0530

    remove expo error

--- mobile/app/Sign-in.tsx
+++ mobile/app/Sign-in.tsx
@@ -1,8 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useSignIn } from '@clerk/clerk-expo';
+import { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
 
-const SignInScreen = () => {
-  return <Signi />
+export default function SignInPage() {
+  const { signIn, setActive, isLoaded } = useSignIn();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSignIn = async () => {
+    if (!isLoaded) return;
+
+    try {
+      const result = await signIn.create({
+        identifier: email,
+        password,
+      });
+
+      await setActive({
+        session: result.createdSessionId,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button title="Sign In" onPress={onSignIn} />
+    </View>
+  );
 }
-
-export default SignInScreen
