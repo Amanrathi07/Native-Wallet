9d2e0d2ea0e2dbec3828c842dae4528f3ef58992
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-31 00:05:12 +0530

    not working

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,15 +1,7 @@
 import { ClerkProvider } from "@clerk/expo";
 import { Stack } from "expo-router";
-import * as SecureStore from "expo-secure-store";
 
-const tokenCache = {
-  async getToken(key: string) {
-    return SecureStore.getItemAsync(key);
-  },
-  async saveToken(key: string, value: string) {
-    return SecureStore.setItemAsync(key, value);
-  },
-};
+
 
 export default function RootLayout() {
   return (
@@ -17,7 +9,7 @@ export default function RootLayout() {
       publishableKey={
         process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
       }
-      tokenCache={tokenCache}
+     
     >
       <Stack />
     </ClerkProvider>
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -5,6 +5,7 @@ import { Text, TouchableOpacity, View } from 'react-native';
 export default function Home() {
   const { isSignedIn } = useAuth();
   const { signOut } = useClerk();
+  console.log(isSignedIn)
   if (!isSignedIn) {
     return (
       <View>
