98805e7c94f8fe642fcdd72bfa83af739e31695c
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-30 23:57:17 +0530

    signup page

--- mobile/app/Sign-up.tsx
+++ mobile/app/Sign-up.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useSignUp } from '@clerk/clerk-expo';
+import { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
 
-export default function SignUpScreen(){
+export default function SignUpPage() {
+  const { signUp, isLoaded } = useSignUp();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSignUp = async () => {
+    if (!isLoaded) return;
+
+    try {
+      const result = await signUp.create({
+        emailAddress: email,
+        password,
+      });
+
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <View>
-      <Text>Sign-up</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        placeholder="Password"
+        value={password}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button title="Sign Up" onPress={onSignUp} />
     </View>
-  )
+  );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,17 @@
 import { useAuth, useClerk } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function Home() {
   const { isSignedIn } = useAuth();
   const { signOut } = useClerk();
   if (!isSignedIn) {
-    return null;
+    return (
+      <View>
+        <Text>pls login </Text>
+        <Link href={"/Sign-in"}>signin</Link>
+      </View>
+    );
   }
 
   return (
